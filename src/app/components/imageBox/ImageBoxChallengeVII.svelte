5fac8a6d5a0d6eeb5f6855cb1fed1394502b5ddb
William <willianpcastro@hotmail.com>
2020-06-24 23:29:06 -0300

    :sparkles: new elements and imagens

---
+++ src/app/components/imageBox/ImageBoxChallengeVII.svelte
@@ -0,0 +1,12 @@
+<script>
+    let basePath = './images/images_challenge_VII/';
+    let typeImage = '.jpeg';
+    let imageNames = [ ...Array (24).keys () ];
+</script>
+<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 p-4 md:p-8">
+    {#each imageNames as imageName}
+        <div class="m-4 justify-self-center shadow-bottom h-026 w-018">
+            <div class="hover:shadow-2xl h-full" style="background-image:url({basePath}{imageName}{typeImage}); background-size: cover;"></div>
+        </div>
+    {/each}
+</div>
--- src/app/views/Home.svelte
+++ src/app/views/Home.svelte
@@ -1,11 +1,20 @@
 <script>
     import Header from '../components/header/Header.svelte';
-    import ImageBox from '../components/imageBox/ImageBox.svelte';
+    import ImageBoxChallengeVI from '../components/imageBox/ImageBoxChallengeVI.svelte';
+    import ImageBoxChallengeVII from '../components/imageBox/ImageBoxChallengeVII.svelte';
     import Footer from "../components/footer/Footer.svelte";
 </script>
 
 <Header/>
 <div class="md:max-w-screen-lg max-w-screen-md contents">
-    <ImageBox/>
+    <div class="flex flex-row items-center justify-center font-Righteous text-rc-v2-orange w-full mt-8">
+        <div class="text-2xl md:text-4xl uppercase">Challenge - VI</div>
+    </div>
+    <ImageBoxChallengeVI/>
+    <div class="flex flex-row items-center justify-center font-Righteous text-rc-v2-orange w-full my-8">
+        <div class="text-2xl md:text-4xl uppercase">Challenge - VII</div>
+    </div>
+
+    <ImageBoxChallengeVII/>
 </div>
 <Footer/>
