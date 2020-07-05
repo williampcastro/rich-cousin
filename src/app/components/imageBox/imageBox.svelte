8022eb7b6a20d95e2061b5c752463f0362927902
will <willianpcastro@hotmail.com>
2020-07-05 11:48:08 -0300

    :sparkles: add jquery and magnific popup

--- src/app/components/imageBox/imageBox.svelte
+++ src/app/components/imageBox/imageBox.svelte
@@ -1,14 +1,26 @@
 <script>
+    import jQuery from 'jquery';
     export let path;
     export let typeImage;
     export let totalImages;
     let basePath = './images/' + path + '/';
     let imageNames = [ ...Array (totalImages).keys () ];
+
+    function handleClick (){
+        jQuery('.parent-div').magnificPopup({
+            delegate: 'a',
+            type: 'image',
+            gallery:{
+                enabled:true
+            }
+        });
+    }
+
 </script>
-<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 p-4 md:p-8">
+<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 p-4 md:p-8 parent-div">
     {#each imageNames as imageName}
-        <div class="m-4 justify-self-center shadow-bottom h-026 w-018">
+        <a on:click={handleClick} href="{basePath}{imageName}{typeImage}" class="m-4 justify-self-center shadow-bottom h-026 w-018">
             <div class="hover:shadow-2xl h-full" style="background-image:url({basePath}{imageName}{typeImage}); background-size: cover;"></div>
-        </div>
+        </a>
     {/each}
 </div>
