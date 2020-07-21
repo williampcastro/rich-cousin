0807726470b75ec562e3ac9b293248b7a1e8db4d
will <willianpcastro@hotmail.com>
2020-07-21 18:42:47 -0300

    :sparkles: add new images

--- src/app/views/Home.svelte
+++ src/app/views/Home.svelte
@@ -11,7 +11,7 @@
     <TitleChallenge title={'Challenge - Rei Salomão'}/>
     <ImageBox path={'images_challenge_salomao'}
               typeImage={'.jpeg'}
-              totalImages={35}/>
+              totalImages={43}/>
 
     <TitleChallenge title={'Challenge - A Arca'}/>
     <ImageBox path={'images_challenge_arca'}
