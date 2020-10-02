9a8a5f971c392941af7f35efedb31a141e09fb01
will <willianpcastro@hotmail.com>
2020-10-02 18:07:11 -0300

    :sparkles: Add new images

--- src/app/views/Home.svelte
+++ src/app/views/Home.svelte
@@ -12,7 +12,7 @@
     <TitleChallenge title={'Challenge - O LEGADO'}/>
     <ImageBox path={'images_challenge_o_legado'}
               typeImage={'.jpeg'}
-              totalImages={7}/>
+              totalImages={10}/>
 
     <TitleChallenge title={'Live - A ARCA'}/>
     <ImageBox path={'images_live_arca'}
